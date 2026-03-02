d53be0aef1352ac5d292247723b2a827487541be
truonghoang_11 <hoangdepzai346@gmail.com>
2026-03-02 17:11:22 +0700

    fix as 2

--- AS2/src/services/api.js
+++ AS2/src/services/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const lessonService = {
   getAllLessons: () => axios.get(API_URL),
   getLessonById: (id) => axios.get(`${API_URL}/${id}`),
   createLesson: (lesson) => axios.post(API_URL, lesson),
   updateLesson: (id, lesson) => axios.put(`${API_URL}/${id}`, lesson),
-  deleteLesson: (id) => axios.delete(`${API_URL}/${id}`)
+  deleteLesson: (id) => axios.delete(`${API_URL}/${id}`),
 };
